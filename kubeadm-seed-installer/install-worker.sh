a2e4966974d1bd1be95bb4b763d18aeabd2717b8
Kamil Domański <kamil@domanski.co>
2018-05-09 19:32:21 +0200

    refactor the install script to make it faster and more organised

--- kubeadm-seed-installer/install-worker.sh
+++ kubeadm-seed-installer/install-worker.sh
@@ -4,25 +4,19 @@ source ./config.sh
 source ./functions.sh.include
 
 # Setup Workers
+echo "Creating workers"
 for ((i = 0; i < ${#WORKER_PUBLIC_IPS[@]}; i++)); do
-        echo "Create Worker $i"
-        TOKEN=$(ssh ${DEFAULT_LOGIN_USER}@${MASTER_PUBLIC_IPS[0]} "sudo kubeadm token create --print-join-command")
+  echo "  ${i}: ${WORKER_PUBLIC_IPS[$i]}"
+  TOKEN=$(ssh ${DEFAULT_LOGIN_USER}@${MASTER_PUBLIC_IPS[0]} "sudo kubeadm token create --print-join-command")
 
-        ssh ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]} "mkdir -p ~/etc/kubernetes"
-        ssh ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]} "sudo mkdir -p /etc/kubernetes"
-        scp ./install-kubeadm-ubuntu.sh ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]}:~/etc/kubernetes/install-kubeadm-ubuntu.sh
-        scp ${CLOUD_CONFIG_FILE} ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]}:~/etc/kubernetes/cloud-config
-        ssh ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]} "sudo cp ~/etc/kubernetes/* /etc/kubernetes/; sudo chown -R root:root /etc/kubernetes/"
+  ssh "${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]}" sudo mkdir -p /etc/kubernetes/ /etc/systemd/system/kubelet.service.d/
+  rsync --rsync-path="sudo rsync" "${CLOUD_CONFIG_FILE}" "${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]}:/etc/kubernetes/cloud-config"
+  rsync --rsync-path="sudo rsync" ./10-hostname.conf "${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]}:/etc/systemd/system/kubelet.service.d/10-hostname.conf"
 
-        scp ./10-hostname.conf ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]}:~/10-hostname.conf
-        ssh ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]} "sudo mkdir -p /etc/systemd/system/kubelet.service.d/"
-        ssh ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]} "sudo cp ~/10-hostname.conf /etc/systemd/system/kubelet.service.d/; sudo chown -R root:root /etc/systemd/system/kubelet.service.d/"
+  install_kubeadm "${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]}"
+  ssh ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]} "sudo ${TOKEN}"
+  # TODO(realfake) On all workers:
+  for (( retry_count = 0; retry_count < 10; retry_count++ )); do ssh ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]} "sudo sed -i 's#server:.*#server: https://'"${MASTER_LOAD_BALANCER_ADDRS[0]}"':6443#g' /etc/kubernetes/kubelet.conf" && break || sleep 10; done
 
-        install_kubeadm "${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]}"
-        ssh ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]} "sudo ${TOKEN}"
-        # TODO(realfake) On all workers:
-        for (( retry_count = 0; retry_count < 10; retry_count++ )); do ssh ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]} "sudo sed -i 's#server:.*#server: https://'"${MASTER_LOAD_BALANCER_ADDRS[0]}"':6443#g' /etc/kubernetes/kubelet.conf" && break || sleep 10; done
-
-        ssh ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]} "sudo systemctl daemon-reload"
-        ssh ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]} "sudo systemctl restart kubelet"
+  ssh ${DEFAULT_LOGIN_USER}@${WORKER_PUBLIC_IPS[$i]} "sudo systemctl daemon-reload && sudo systemctl restart kubelet"
 done
