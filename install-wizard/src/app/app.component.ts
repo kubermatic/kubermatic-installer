5f3c6d743673fa7bbec919f8c0b19bd9d8fbc8c8
Christoph Mewes <christoph@loodse.com>
2018-09-12 11:55:12 +0200

    add export button to download the current manifest

--- install-wizard/src/app/app.component.ts
+++ install-wizard/src/app/app.component.ts
@@ -8,4 +8,28 @@ import { Manifest } from './manifest.class';
 })
 export class AppComponent {
   manifest = new Manifest();
+
+  exportManifest(): void {
+    let data = this.manifest;
+    data.created = new Date();
+    data.appVersion = 1;
+
+    this.download("manifest.json", JSON.stringify(data));
+  }
+
+  // from https://stackoverflow.com/a/18197511
+  download(filename, text): void {
+    let pom = document.createElement('a');
+    pom.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+
+    if (document.createEvent) {
+      let event = document.createEvent('MouseEvents');
+      event.initEvent('click', true, true);
+      pom.dispatchEvent(event);
+    }
+    else {
+      pom.click();
+    }
+  }
 }
--- install-wizard/src/app/manifest.class.ts
+++ install-wizard/src/app/manifest.class.ts
@@ -6,4 +6,8 @@ export class Manifest {
   cloudProvider: string = "";
   name: string = "";
   cloudConfig: string = "";
+
+  // used when downloading the manifest
+  created: Date;
+  appVersion: number;
 }
