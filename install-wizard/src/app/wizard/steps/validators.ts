745c9f7ae2af706f450c94ae0592939990c5b503
Christoph Mewes <github@xrstf.de>
2018-09-13 13:49:52 +0200

    Add manifest import/export buttons (#58)

--- install-wizard/src/app/wizard/steps/validators.ts
+++ install-wizard/src/app/wizard/steps/validators.ts
@@ -1,8 +1,8 @@
-import { FormControl } from "@angular/forms";
+import { FormControl } from '@angular/forms';
 
 export function Required(control: FormControl) {
   if (control.value.length === 0) {
-    return {required: "This is a required field."};
+    return {required: 'This is a required field.'};
   }
 
   return null;
