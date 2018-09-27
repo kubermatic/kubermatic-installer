83b621c25c52270c4d48423dc33c4e53e425cef2
Christoph Mewes <christoph@loodse.com>
2018-09-27 19:12:25 +0200

    implement authorization step

--- install-wizard/src/app/wizard/wizard.component.ts
+++ install-wizard/src/app/wizard/wizard.component.ts
@@ -43,7 +43,7 @@ export class WizardComponent implements WizardInterface, OnInit {
       new FinalStepComponent(),
     ];
 
-    this.currentStepIndex = 5;
+    this.currentStepIndex = 0;
     this.stepComponents = [];
     this.stepValid = false;
   }
