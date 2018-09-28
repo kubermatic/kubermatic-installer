e46b6d6010d5baf155ece37079cc7ec3a1c05d5b
Christoph Mewes <github@xrstf.de>
2018-09-28 09:41:29 +0200

    implement authorization step (#70)

--- install-wizard/src/app/wizard/steps/authorization/step.component.ts
+++ install-wizard/src/app/wizard/steps/authorization/step.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Step } from '../step.class';
+import { MatCheckboxChange } from '@angular/material';
 
 @Component({
   selector: 'authorization-step',
@@ -8,7 +10,28 @@ import { Step } from '../step.class';
 })
 export class AuthorizationStepComponent extends Step implements OnInit {
   ngOnInit(): void {
-    this.onEnter();
+    const github = this.manifest.authorization.github;
+    const google = this.manifest.authorization.google;
+
+    const form = new FormGroup({
+      github: new FormGroup({
+        enabled: new FormControl(github.isEnabled()),
+        clientID: new FormControl({value: github.clientID, disabled: !github.isEnabled()}),
+        secretKey: new FormControl({value: github.secretKey, disabled: !github.isEnabled()}),
+        organization: new FormControl({value: github.organization, disabled: !github.isEnabled()}),
+      }),
+      google: new FormGroup({
+        enabled: new FormControl(google.isEnabled()),
+        clientID: new FormControl({value: google.clientID, disabled: !google.isEnabled()}),
+        secretKey: new FormControl({value: google.secretKey, disabled: !google.isEnabled()}),
+      }),
+    });
+
+    this.defineForm(
+      form,
+      () => this.validateManifest(),
+      (values) => this.updateManifestFromForm(values)
+    );
   }
 
   onEnter(): void {
@@ -21,5 +44,40 @@ export class AuthorizationStepComponent extends Step implements OnInit {
 
   isAdvanced(): boolean {
     return false;
+  }
+
+  validateManifest(): any {
+    if (!this.manifest.authorization.github.isEnabled() && !this.manifest.authorization.google.isEnabled()) {
+      return {noApp: 'You must enable at least one authentication provider.'};
+    }
+
+    return null;
+  }
+
+  updateManifestFromForm(values): void {
+    this.manifest.authorization.github.clientID = values.github.clientID || '';
+    this.manifest.authorization.github.secretKey = values.github.secretKey || '';
+    this.manifest.authorization.github.organization = values.github.organization || '';
+
+    this.manifest.authorization.google.clientID = values.google.clientID || '';
+    this.manifest.authorization.google.secretKey = values.google.secretKey || '';
+  }
+
+  onProviderCheckboxChange(provider, event: MatCheckboxChange): void {
+    Object.entries((<FormGroup>this.form.controls[provider]).controls).forEach(([name, control]) => {
+      if (name === 'enabled') {
+        return;
+      }
+
+      if (event.checked) {
+        control.enable();
+      } else {
+        control.disable();
+      }
+    });
+  }
+
+  onProviderCheckboxClick(event: MouseEvent): void {
+    event.stopPropagation();
   }
 }
