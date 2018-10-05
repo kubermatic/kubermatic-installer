2ea1a9ae65b31c406111473ef417d840ea3f16d2
Christoph Mewes <christoph@loodse.com>
2018-10-05 15:36:17 +0200

    show required DNS changes on the final wizard screen

--- install-wizard/src/app/wizard/steps/final-screen/step.component.ts
+++ install-wizard/src/app/wizard/steps/final-screen/step.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Step } from '../step.class';
 
+class DNSRecord {
+  constructor(public name: string, public type: string, public target: string) {}
+}
+
 @Component({
   selector: 'final-step',
   templateUrl: './step.component.html',
   styleUrls: ['./step.component.scss']
 })
 export class FinalStepComponent extends Step implements OnInit {
+  dnsRecords: DNSRecord[];
+
   ngOnInit(): void {
     this.onEnter();
   }
@@ -14,6 +20,23 @@ export class FinalStepComponent extends Step implements OnInit {
   onEnter(): void {
     this.wizard.setValid(true);
     this.wizard.setAllowBack(false);
+
+    const result = this.wizard.getInstallationResult();
+    const domain = this.manifest.settings.baseDomain;
+    const seed = this.manifest.seedClusters[0];
+
+    this.dnsRecords = [];
+
+    if (result.nginxIngresses.length > 0) {
+      const first = result.nginxIngresses[0];
+      this.dnsRecords.push(this.dnsRecord(domain, first));
+      this.dnsRecords.push(this.dnsRecord('*.' + domain, first));
+    }
+
+    if (result.nodeportIngresses.length > 0) {
+      const first = result.nodeportIngresses[0];
+      this.dnsRecords.push(this.dnsRecord('*.' + seed + '.' + domain, first));
+    }
   }
 
   getStepTitle(): string {
@@ -50,5 +73,13 @@ export class FinalStepComponent extends Step implements OnInit {
 
   fullDomain(): string {
     return this.manifest.settings.baseDomain;
+  }
+
+  dnsRecord(name: string, target: any): DNSRecord {
+    if (target.ip) {
+      return new DNSRecord(name, 'A', target.ip);
+    } else {
+      return new DNSRecord(name, 'CNAME', target.hostname);
+    }
   }
 }
