bef710c601f75cc3eba415d04b8b5509b15e78a9
Prow CI Robot <dev@loodse.com>
2019-04-24 12:49:02 +0000

    Syncing charts from release v2.10.0-rc3

--- charts/monitoring/grafana/dashboards/cleanup.sh
+++ charts/monitoring/grafana/dashboards/cleanup.sh
@@ -11,13 +11,22 @@ for dashboard in */*.json; do
     jq '(.templating.list[] | select(.type=="query") | .options) = []' | \
     jq '(.templating.list[] | select(.type=="query") | .refresh) = 2' | \
     jq '(.templating.list[] | select(.type=="query") | .current) = {}' | \
+    jq '(.templating.list[] | select(.type=="datasource") | .current) = {}' | \
+    jq '(.templating.list[] | select(.type=="datasource") | .hide) = "<< datasourceHide | toJson >>"' | \
     jq '(.annotations.list) = []' | \
     jq '(.links) = []' | \
     jq '(.refresh) = "<< refresh | default `30s` | toJson >>"' | \
     jq '(.time.from) = "<< defaultRange | toJson >>"' | \
+    jq '(.editable) = "<< editable | default false | toJson >>"' | \
+    jq '(.panels[] | select(.type!="row") | .editable) = "<< editable | default false | toJson >>"' | \
+    jq '(.panels[] | select(.type!="row") | .transparent) = "<< transparentPanels | default true | toJson >>"' | \
+    jq '(.panels[] | select(.type!="row") | .timeRegions) = []' | \
+    jq '(.hideControls) = "<< hideControls | default false | toJson >>"' | \
     jq '(.time.to) = "now"' | \
     jq '(.timezone) = ""' | \
     jq '(.graphTooltip) = 1' | \
+    jq 'del(.id)' | \
+    jq 'del(.iteration)' | \
     jq --sort-keys '.' > "$tmpfile"
 
   mv "$tmpfile" "$dashboard"
