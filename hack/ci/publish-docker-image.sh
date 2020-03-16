f0adb8c076abdf2cea8f06268d8a4040e761cf4b
Christoph Mewes <christoph@loodse.com>
2020-03-16 14:51:42 +0100

    add quay integration

--- hack/ci/publish-docker-image.sh
+++ hack/ci/publish-docker-image.sh
@@ -3,22 +3,27 @@
 set -euo pipefail
 cd $(dirname $0)/../..
 
-echodate() {
-  echo "[$(date -Is)]" "$@"
-}
+source ./hack/lib.sh
+
+if [ -n "${QUAY_IO_USERNAME:-}" ]; then
+  echodate "Logging into Quay"
+  docker ps > /dev/null 2>&1 || start-docker.sh
+  retry 5 docker login -u "$QUAY_IO_USERNAME" -p "$QUAY_IO_PASSWORD" quay.io
+  echodate "Successfully logged into Quay"
+fi
 
 repo=quay.io/kubermatic/kubermatic-installer
 
 image="$repo:$(git rev-parse HEAD)"
 
-echo "Building Docker image $image"
+echodate "Building Docker image $image"
 docker build -t "$image" .
 docker push -t "$image"
 
 tag="$(git tag -l --points-at HEAD)"
 if [ -n "$tag" ]; then
   taggedImage="$repo:$tag"
-  echo "Re-tagging $image as $taggedImage"
+  echodate "Re-tagging $image as $taggedImage"
   docker tag "$image" "$taggedImage"
   docker push "$taggedImage"
 fi
