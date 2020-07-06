579239234d57a3b83d43fcafa214973626b42ba3
Prow CI Robot <dev@loodse.com>
2020-07-06 11:27:29 +0000

    Syncing charts from release weekly-2020-27

--- charts/monitoring/prometheus/rules/src/compile.sh
+++ charts/monitoring/prometheus/rules/src/compile.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env sh
 
+# Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 cd $(dirname $0)
 
 # Install yq if not installed
@@ -15,5 +29,5 @@ comment="# This file has been generated, do not edit."
 for file in */*.yaml; do
   newfile=$(dirname $file)-$(basename $file)
   echo "$file => $newfile"
-  yq r $file -j | jq 'del(.groups[].rules[].runbook)' | (echo "$comment"; yq r -) > ../$newfile
+  yq r $file --tojson | jq 'del(.groups[].rules[].runbook)' | (echo "$comment"; yq r --prettyPrint -) > ../$newfile
 done
